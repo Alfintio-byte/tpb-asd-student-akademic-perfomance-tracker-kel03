d55cb524ca720dcc60d3cab0ae042c8a9bbe0d9b
Alfintio-byte <septianraihan61@gmail.com>
2026-05-18 16:52:06 +0700

    update student features

--- src/src/main.py
+++ src/src/main.py
@@ -10,13 +10,28 @@ while True:
 
     if pilih == "1":
         nama = input("Nama mahasiswa: ")
-        nilai = int(input("Nilai: "))
 
-        students.append([nama, nilai])
+        tugas = int(input("Nilai tugas: "))
+        uts = int(input("Nilai UTS: "))
+        uas = int(input("Nilai UAS: "))
+
+        rata = (tugas + uts + uas) / 3
+
+        if rata >= 75:
+            status = "Lulus"
+        else:
+            status = "Tidak Lulus"
+
+        students.append([nama, rata, status])
 
     elif pilih == "2":
+        print("\n=== DATA MAHASISWA ===")
+
         for s in students:
-            print("Nama:", s[0], "| Nilai:", s[1])
+            print("Nama :", s[0])
+            print("Rata-rata :", s[1])
+            print("Status :", s[2])
+            print()
 
     elif pilih == "3":
         break
