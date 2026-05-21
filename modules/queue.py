3bb281628a6cc517c589de78700be18e06094fef
Fahmy Ferdiansyah <fahmy2ft.2025.student.uny.ac.id>
2026-05-22 00:25:58 +0700

    feat(queue): update queue

---
+++ modules/queue.py
@@ -0,0 +1,9 @@
+class Queue:
+
+    def __init__(self):
+
+        self.items = []
+
+    def enqueue(self, data):
+
+        self.items.append(data)
---
+++ src/queue.py
@@ -0,0 +1,25 @@
+class Queue:
+
+    def __init__(self):
+
+        self.items = []
+
+    def enqueue(self, data):
+
+        self.items.append(data)
+
+    def dequeue(self):
+
+        if not self.is_empty():
+
+            return self.items.pop(0)
+
+        return None
+
+    def is_empty(self):
+
+        return len(self.items) == 0
+
+    def display(self):
+
+        print("Queue :", self.items)
